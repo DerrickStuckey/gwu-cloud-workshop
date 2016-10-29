ec7fb86c8c32332c27d4280ba1b7e19e0cc53fb9
Derrick Stuckey <derrickstuckey@gmail.com>
2016-10-29 17:55:34 -0400

    fix filename

--- r_scripts/s3_read.R
+++ r_scripts/s3_read.R
@@ -1,6 +1,6 @@
 ## read CSV from S3
 
-us_10year_rates <- read.table("http://s3.amazonaws.com/stuckeys-bucket/us_10year_rates.csv",
+us_10year_rates <- read.table("http://s3.amazonaws.com/stuckeys-bucket/us_interest_rates.csv",
                               header=TRUE, sep=",")
 
 summary(us_10year_rates)
