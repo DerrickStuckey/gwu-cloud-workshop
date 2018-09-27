9a979c93849890e3af0a62c04e74cb527cbc6221
Derrick <dstuckey@lotame.com>
2018-09-27 16:56:16 -0400

    commented up

--- r_scripts/s3_read_advanced.R
+++ r_scripts/s3_read_advanced.R
@@ -3,6 +3,9 @@ install.packages("xml2")
 # y/n: n
 
 install.packages("aws.s3")
+# if that fails try:
+# install.packages("aws.s3", repos = c("cloudyr" = "http://cloudyr.github.io/drat"), INSTALL_opts = "--no-multiarch")
+
 library("aws.s3")
 
 # obtain from ~/.aws/credentials
@@ -10,12 +13,17 @@ Sys.setenv("AWS_ACCESS_KEY_ID" = "",
            "AWS_SECRET_ACCESS_KEY" = "",
            "AWS_DEFAULT_REGION" = "us-east-1")
 
+# list all the buckets for this account
 bucketlist()
-get_bucket(bucket = 'gwu-workshop-stuckey')
 
-# textdata <- readBin(con=get_object("s3://gwu-workshop-stuckey/us_interest_rates.csv"),what=character())
+# list all the s3 objects in bucket 'gwu-workshop-stuckey'
+workshop_bucket <- get_bucket(bucket = 'gwu-workshop-stuckey')
+for (obj in workshop_bucket) {
+  key <- obj$Key
+  print(key)
+}
 
-us_interest_rates <- s3read_using(read.csv, object="s3://gwu-workshop-stuckey/us_interest_rates.csv")
-us_interest_rates
+# read the object we are interested in, "us_interest_rates.csv", as a dataframe, using read.csv
+us_interest_rates <- s3read_using(read.csv, object="us_interest_rates.csv", bucket="gwu-workshop-stuckey")
 head(us_interest_rates)
 
